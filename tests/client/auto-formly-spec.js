1460f30ba663296e794a8b3c43b9fc791e61b67d
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2015-11-23 16:40:38 +0100

    test(autoFormly): schema filtering using array or objects with formly field configuration

--- tests/client/auto-formly-spec.js
+++ tests/client/auto-formly-spec.js
@@ -10,21 +10,56 @@ describe('autoFormly', () => {
     });
 
     describe("filterSchema()", () => {
-        it("should fail when fields are not an array", () => {
-            const values = ["s", "", 0, 1, true, false, undefined, null, {}, () => {
+        it("should fail when fields are not an array or object", () => {
+            const values = ["s", "", 0, 1, true, false, undefined, null, () => {
             }];
 
             values.forEach((val) => {
                 expect(() => {
                     autoFormly.filterSchema({}, val);
-                }).toThrowError(Error, "[AutoFormly] Fields have to be an array");
+                }).toThrowError(Error, "[AutoFormly] Fields have to be an array or object");
             });
         });
-        it("should filter correctly", () => {
+
+        it('should pass on array fields', () => {
+            expect(() => {
+                autoFormly.filterSchema({}, []);
+            }).not.toThrowError(Error, "[AutoFormly] Fields have to be an array or object");
+        });
+
+        it('should pass on object fields', () => {
+            expect(() => {
+                autoFormly.filterSchema({}, {});
+            }).not.toThrowError(Error, "[AutoFormly] Fields have to be an array or object");
+        });
+
+        it("should filter correctly using array", () => {
             const fields = autoFormly.filterSchema(UserSchema, ['username', 'createdAt']);
             expect(fields.username).toBeDefined();
             expect(fields.createdAt).toBeDefined();
             expect(fields.services).toBeUndefined();
+        });
+
+        it("should filter correctly using object", () => {
+            const fields = autoFormly.filterSchema(UserSchema, {
+                username: true,
+                createdAt: true,
+                services: false
+            });
+            expect(fields.username).toBeDefined();
+            expect(fields.createdAt).toBeDefined();
+            expect(fields.services).toBeUndefined();
+        });
+
+        it("should extend autoformly configuration in schema", () => {
+            const fields = autoFormly.filterSchema(UserSchema, {
+                username: {
+                    templateOptions: {
+                        label: "Test label"
+                    }
+                }
+            });
+            expect(fields.username.autoformly.templateOptions.label).toBe("Test label");
         });
     });
 
